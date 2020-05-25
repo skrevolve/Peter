7acdeac423e4ff83ef67b2c6890083b5a6f807d0
limseokyu <41939976+skrevolve@users.noreply.github.com>
2020-05-25 09:51:54 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	//about 페이지 이동 이벤트
+    $('#GoToabout1').click(function(){
+        var offset = $('#about').offset(); //선택한 태그의 위치를 반환
+        $('html').animate({scrollTop : offset.top}, 500);
+	});
+	
+	//about 페이지 이동 이벤트(하단 화살표)
+	$('#GoToabout2').click(function(){
+        var offset = $('#about').offset();
+        $('html').animate({scrollTop : offset.top}, 500);
+	});
+	
+	//skills 페이지 이동 이벤트
+	$('#GoToskills').click(function(){
+        var offset = $('#skills').offset();
+        $('html').animate({scrollTop : offset.top}, 500);
+	});
+	
+	//projects 페이지 이동 이벤트
+	$('#GoToprojects').click(function(){
+        var offset = $('#projects').offset();
+        $('html').animate({scrollTop : offset.top}, 500);
+	});
+
+	$('.parallax-window').parallax({imageSrc: 'img/test.jpg',iosFix: false,androidFix: false});
+
+	// $('.parallax-window').parallax({imageSrc: 'img/test.jpg'});
+	// jQuery(window).trigger('resize').trigger('scroll');
+});
+//# sourceURL=index.js
